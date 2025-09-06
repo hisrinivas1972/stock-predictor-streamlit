7d207f69f7c87e69da9c1ef7b63988103f1bf398
hisrinivas1972 <104999742+hisrinivas1972@users.noreply.github.com>
2025-09-06 17:25:17 +0530

    Create forecasting.py

---
+++ utils/forecasting.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from prophet import Prophet
+import yfinance as yf
+
+def make_forecast(ticker, timeframe):
+    stock = yf.Ticker(ticker)
+    info = stock.info
+    currency = info.get("currency", "USD")
+    df = stock.history(period="1y")[["Close"]].reset_index()
+    df.columns = ["ds", "y"]
+
+    model = Prophet(daily_seasonality=True)
+    model.fit(df)
+
+    days = {'1 day': 1, '1 week': 7, '1 month': 30, '3 months': 90}[timeframe]
+    future = model.make_future_dataframe(periods=days)
+    forecast = model.predict(future)
+
+    predicted = forecast.iloc[-1]['yhat']
+    lower = forecast.iloc[-1]['yhat_lower']
+    upper = forecast.iloc[-1]['yhat_upper']
+
+    return forecast, df, currency, predicted, lower, upper
